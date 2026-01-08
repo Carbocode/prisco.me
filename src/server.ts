3cdb324fbbfbf5fd15e2006f184ef10d052494e6
Carbocode <noreply@prisco.me>
2026-01-08 20:29:20 +0100

    test fix cloudflare build

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,18 @@
-import { wrapFetchWithSentry } from "@sentry/tanstackstart-react";
-import handler, { createServerEntry } from "@tanstack/react-start/server-entry";
+import * as Sentry from "@sentry/cloudflare";
+import handler from "@tanstack/react-start/server-entry";
 
-export default createServerEntry(
-	wrapFetchWithSentry({
-		fetch(request: Request) {
+export default Sentry.withSentry(
+	(env: any) => {
+		const { id: versionId } = env.CF_VERSION_METADATA;
+		return {
+			dsn: "https://5612ec342f3bba5f99d97f79453e2ddd@o4510675457540096.ingest.de.sentry.io/4510675466125392",
+			release: versionId,
+			sendDefaultPii: true,
+		};
+	},
+	{
+		async fetch(request: Request) {
 			return handler.fetch(request);
 		},
-	}),
+	},
 );
